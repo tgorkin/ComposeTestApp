22c54d1830003e670bb76f89b141d2c4805ba3da
Travis Gorkin <tgorkin@google.com>
2022-07-13 12:56:29 -0700

    Update to compose 1.2.0-rc03, kotlin 7.2.1, cleanup dependencies.

--- app/src/main/java/com/tgorkin/composetestapp/MainActivity.kt
+++ app/src/main/java/com/tgorkin/composetestapp/MainActivity.kt
@@ -19,7 +19,6 @@ import android.app.TimePickerDialog
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -304,7 +303,6 @@ fun ScrollingListScreen(navController: NavController? = null) {
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-@OptIn(ExperimentalFoundationApi::class)
 fun ScrollingGridScreen(navController: NavController? = null) {
     Surface(color = MaterialTheme.colors.background) {
         Column {
